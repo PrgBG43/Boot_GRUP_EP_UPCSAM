2d7ea24d0129b273945172b804c8851e3db0b3fb
juanibanez2006 <juibnodega@gmail.com>
2026-04-08 19:27:27 -0500

    formatting.py

--- app/schemas/role.py
+++ app/schemas/role.py
@@ -2,20 +2,16 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-
 class RoleBase(BaseModel):
     name: str
     description: Optional[str] = None
 
-
 class RoleCreate(RoleBase):
     pass
-
 
 class RoleUpdate(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
-
 
 class RoleResponse(RoleBase):
     id: int
